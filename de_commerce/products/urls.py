04ee4e1def2f58e90b3e1d16e07fd46a2b4736f3
CMNDEGWA <muthunguclintn@gmail.com>
2026-02-11 18:13:31 +0300

    Update: App-level URL routing.

--- de_commerce/products/urls.py
+++ de_commerce/products/urls.py
@@ -1,8 +1,17 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
+from rest_framework.routers import DefaultRouter
+from .api import CategoryViewSet, ProductViewSet, CartViewSet, OrderViewSet
+
 
 app_name = 'products'
+
+router = DefaultRouter()
+router.register(r'api/categories', CategoryViewSet, basename='category')
+router.register(r'api/products', ProductViewSet, basename='product')
+router.register(r'api/carts', CartViewSet, basename='cart')
+router.register(r'api/orders', OrderViewSet, basename='order')
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -13,8 +22,9 @@ urlpatterns = [
     path('place-order/', views.place_order, name='place_order'),
     path('register/', views.register, name='register'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
-    path('logout/', views.profile, name='logout'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
     path('profile/', views.profile, name='profile'),
     path('orders/', views.order_history, name='order_history'),
     path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
+    path('', include(router.urls)),
 ]
