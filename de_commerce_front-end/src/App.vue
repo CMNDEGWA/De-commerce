5fb20e4a745c3019a7780e05398e0cdb0b3795cd
CMNDEGWA <muthunguclintn@gmail.com>
2026-02-13 11:54:10 +0300

    Update: Footer linking.

--- de_commerce_front-end/src/App.vue
+++ de_commerce_front-end/src/App.vue
@@ -5,10 +5,12 @@
     <main>
       <router-view />
     </main>
+    <Footer />
   </div>
 </template>
 <script setup>
 import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 
