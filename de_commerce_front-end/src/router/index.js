ad4ff179d30aee2a2a3d5622066a7ef2eb5ad0ad
CMNDEGWA <muthunguclintn@gmail.com>
2026-02-23 07:28:39 +0300

    Update: Route Linkage.

--- de_commerce_front-end/src/router/index.js
+++ de_commerce_front-end/src/router/index.js
@@ -10,6 +10,11 @@ import OrderDetail from '../views/OrderDetail.vue';
 import Profile from '../views/Profile.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
+import ResetPassword from '../views/ResetPassword.vue';
+import CookiePolicy from '../views/CookiePolicy.vue';
+import TermsOfUse from '../views/TermsOfUse.vue';
+import PrivacyPolicy from '../views/PrivacyPolicy.vue';
+import Contact from '../views/Contact.vue';
 import { useAuthStore } from '../store/auth';
 
 const routes = [
@@ -23,6 +28,11 @@ const routes = [
   { path: '/profile', name: 'Profile', component: Profile },
   { path: '/login', name: 'Login', component: Login },
   { path: '/register', name: 'Register', component: Register },
+  { path: '/reset-password', name: 'ResetPassword', component: ResetPassword },
+  { path: '/cookie-policy', name: 'CookiePolicy', component: CookiePolicy },
+  { path: '/terms-of-use', name: 'TermsOfUse', component: TermsOfUse },
+  { path: '/privacy-policy', name: 'PrivacyPolicy', component: PrivacyPolicy },
+  { path: '/contact', name: 'Contact', component: Contact },
 ];
 
 const router = createRouter({
